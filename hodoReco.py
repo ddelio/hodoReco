1e597e7d3292d0e7ef6b593ec35e99d8b3443a4c
Michael O'Donnell <michaeod@ttu.edu>
2025-07-10 14:30:34 -0500

    Update hodoReco.py

--- hodoReco.py
+++ hodoReco.py
@@ -4,17 +4,22 @@ from hodoChannel import hodoChannel
 
 def rootInspect():
     # don't know the structure
+    # Michael
     return
 
 def eventProcess():
+    # Michael
     return
 
 def eventThres():
+    # Jackson
     return
 
 def printPair():
+    # Anyone -- but just wait
     return
 
 def main():
+    # Michael
     return
 
