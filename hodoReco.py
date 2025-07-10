8fe7896c5b191a1d9291fc9ea2977a3c5187b551
Michael O'Donnell <michaeod@ttu.edu>
2025-07-10 14:29:42 -0500

    Create hodoReco.py

---
+++ hodoReco.py
@@ -0,0 +1,20 @@
+import ROOT
+import numpy as np
+from hodoChannel import hodoChannel
+
+def rootInspect():
+    # don't know the structure
+    return
+
+def eventProcess():
+    return
+
+def eventThres():
+    return
+
+def printPair():
+    return
+
+def main():
+    return
+
